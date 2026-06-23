30bef9f18d030f506a90e4c9eceb195c0624de3f
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-06-23 17:53:56 -0600

    refactor: Register the main routes.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,14 @@
 <?php
 
+use Controllers\PropiedadController;
 use Routes\Router;
 
 require_once __DIR__ . '/../includes/app.php';
 
-$route = new Router();
+$router = new Router();
+
+$router->get('/admin', [PropiedadController::class, 'index']);
+$router->get('/propiedades/create', [PropiedadController::class, 'create']);
+$router->get('/propiedades/edit', [PropiedadController::class, 'edit']);
+
+$router->checkRoutes();

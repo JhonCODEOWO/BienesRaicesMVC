c1c499b79d6e8ccdc9ec5986708f06e6a57012b8
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-06-23 17:53:38 -0600

    feat: Implements PropiedadController with its main methods.

---
+++ controllers/PropiedadController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controllers;
+
+class PropiedadController {
+    function index() {
+        echo "index";
+    }
+
+    function create(){
+        echo "crear propiedad";
+    }
+
+    function edit(){
+        echo "crear editar";
+    }
+}

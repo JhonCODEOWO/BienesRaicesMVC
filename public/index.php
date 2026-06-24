e12a0920f6413c6e53fdc1f50b109aec64bdaf72
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-06-24 10:24:31 -0600

    add docs.

--- public/index.php
+++ public/index.php
@@ -6,9 +6,10 @@ use Routes\Router;
 require_once __DIR__ . '/../includes/app.php';
 
 $router = new Router();
-
+//Registering routes.
 $router->get('/admin', [PropiedadController::class, 'index']);
 $router->get('/propiedades/create', [PropiedadController::class, 'create']);
 $router->get('/propiedades/edit', [PropiedadController::class, 'edit']);
 
+//Handling the incoming request.
 $router->checkRoutes();

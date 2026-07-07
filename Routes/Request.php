d569fad075293acc57d25b2f6db3e5ef199e9f4f
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-07-06 18:29:23 -0600

    feat: Implements $files array constructor arg and its methods to convert a input $_FILES to a array of UploadedFile instances.

--- Routes/Request.php
+++ Routes/Request.php
@@ -2,6 +2,8 @@
 
 namespace Routes;
 
+use Core\UploadedFile;
+
 class Request {
     /** Array with all url params in the request */
     private array $urlParams = [];
@@ -11,13 +13,15 @@ class Request {
     private string $method = '';
     /** Body request */
     private array $body = [];
+    private array $files = [];
 
-    public function __construct(array $args)
+    public function __construct(array $args, ?array $files)
     {
         $this->urlParams = $args["urlParams"] ?? [];
         $this->queryParams = $args["queryParams"] ?? [];
         $this->method = $args['method'] ?? '';
         $this->body = $args["body"] ?? [];
+        $this->files = $this->toUploadedFilesArray($files) ?? [];
     }
 
     // =========================
@@ -57,9 +61,13 @@ class Request {
         return $this->body[$key] ?? null;
     }
 
-    public function body(): array
+    public function getBody(): array
     {
         return $this->body;
+    }
+
+    public function body(): array {
+        return array_merge($this->getBody(), ["files" => $this->files]);
     }
 
     // =========================
@@ -82,5 +90,34 @@ class Request {
             'queryParams' => $this->queryParams,
             'body' => $this->body,
         ];
+    }
+
+    public function getFile(string $fileInputName): array{
+        return $this->files[$fileInputName];
+    }
+
+    public function toUploadedFilesArray(array $files): array {
+        $uploadedFiles = [];
+        foreach ($files as $inputFieldName => $filesData) {
+            if(!is_array($filesData['name'])){
+                $uploadedFiles[$inputFieldName][] = new UploadedFile(
+                    $filesData['name'],
+                    $filesData['tmp_name'],
+                    $filesData['error'],
+                    $filesData['type'],
+                );
+                break;
+            };
+            
+            foreach ($filesData['name'] as $index => $name) {
+                $uploadedFiles[$inputFieldName][$index] = new UploadedFile(
+                    $name,
+                    $filesData['tmp_name'][$index],
+                    $filesData['error'][$index],
+                    $filesData['type'][$index],
+                );
+            }
+        }
+        return $uploadedFiles;
     }
 }

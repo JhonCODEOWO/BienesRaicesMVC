2b65cfbd85d8a9e1c21b3ec4c8879afd57ec9f85
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-07-14 15:16:02 -0600

    feat: Adds routes to manage Vendedores.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
 use Controllers\PropiedadController;
+use Controllers\VendedoresController;
+
 use Routes\Router;
 
 require_once __DIR__ . '/../includes/app.php';
@@ -13,6 +15,12 @@ $router->post('/propiedades/create', [PropiedadController::class, 'save']);
 $router->get('/propiedades/edit/{id}', [PropiedadController::class, 'edit']);
 $router->post('/propiedades/update/{id}', [PropiedadController::class, 'update']);
 $router->post('/propiedades/delete/{id}', [PropiedadController::class, 'delete']);
+
+$router->get('/vendedores/create', [VendedoresController::class, 'create']);
+$router->post('/vendedores/create', [VendedoresController::class, 'store']);
+$router->get('/vendedores/edit/{id}', [VendedoresController::class, 'edit']);
+$router->post('/vendedores/update/{id}', [VendedoresController::class, 'update']);
+$router->post('/vendedores/delete/{id}', [VendedoresController::class, 'delete']);
 
 //Handling the incoming request.
 $router->checkRoutes();

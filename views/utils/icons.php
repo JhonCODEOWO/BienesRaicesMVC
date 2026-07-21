5bbdc2cc1dc0c0d15595b10c368ad6ee62e5fd02
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-07-21 13:24:45 -0600

    feat: Creates a util file to render icons.

---
+++ views/utils/icons.php
@@ -0,0 +1,19 @@
+    <div class="grid grid-cols-3 p-3">
+        <div class="icono">
+            <img src="/assets/icono1.svg" alt="Icono Seguridad">
+            <h3>Seguridad</h3>
+            <p>Quasi quibusdam, quos deserunt, recusandae iusto dolorem voluptatibus quaerat veritatis consectetur culpa sit dignissimos maiores iure id, magnam non voluptatum molestiae doloremque.</p>
+        </div>
+
+        <div class="icono">
+            <img src="/assets/icono2.svg" alt="Icono Mejor Precio">
+            <h3>El Mejor Precio</h3>
+            <p>Quasi quibusdam, quos deserunt, recusandae iusto dolorem voluptatibus quaerat veritatis consectetur culpa sit dignissimos maiores iure id, magnam non voluptatum molestiae doloremque.</p>
+        </div>
+
+        <div class="icono">
+            <img src="/assets/icono3.svg" alt="Icono a Tiempo">
+            <h3>A Tiempo</h3>
+            <p>Quasi quibusdam, quos deserunt, recusandae iusto dolorem voluptatibus quaerat veritatis consectetur culpa sit dignissimos maiores iure id, magnam non voluptatum molestiae doloremque.</p>
+        </div>
+    </div>

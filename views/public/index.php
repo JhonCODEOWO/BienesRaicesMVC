833baadffa5e9372c7c7b304e2e5e8f093c3e962
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-07-21 13:25:25 -0600

    feat: Uses new util icons file.

--- views/public/index.php
+++ views/public/index.php
@@ -9,25 +9,7 @@
 <section class="my-5 mx-3">
     <h2 class="text-center">Más Sobre Nosotros</h2>
 
-    <div class="grid grid-cols-3 p-3">
-        <div class="icono">
-            <img src="/assets/icono1.svg" alt="Icono Seguridad">
-            <h3>Seguridad</h3>
-            <p>Quasi quibusdam, quos deserunt, recusandae iusto dolorem voluptatibus quaerat veritatis consectetur culpa sit dignissimos maiores iure id, magnam non voluptatum molestiae doloremque.</p>
-        </div>
-
-        <div class="icono">
-            <img src="/assets/icono2.svg" alt="Icono Mejor Precio">
-            <h3>El Mejor Precio</h3>
-            <p>Quasi quibusdam, quos deserunt, recusandae iusto dolorem voluptatibus quaerat veritatis consectetur culpa sit dignissimos maiores iure id, magnam non voluptatum molestiae doloremque.</p>
-        </div>
-
-        <div class="icono">
-            <img src="/assets/icono3.svg" alt="Icono a Tiempo">
-            <h3>A Tiempo</h3>
-            <p>Quasi quibusdam, quos deserunt, recusandae iusto dolorem voluptatibus quaerat veritatis consectetur culpa sit dignissimos maiores iure id, magnam non voluptatum molestiae doloremque.</p>
-        </div>
-    </div>
+    <?php include __DIR__.'/../utils/icons.php' ?>
 </section>
 
 <main class="seccion contenedor">

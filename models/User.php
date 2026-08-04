fcd429ac981a579dcd37883e6ed080c64cb6cff1
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-08-04 12:52:33 -0600

    hotfix: Fix bad names inside construct function.

--- models/User.php
+++ models/User.php
@@ -3,9 +3,9 @@
 namespace Models;
 
 class User extends ActiveRecord{
-    protected static $table = 'usuarios';
-    protected static $idName = 'idUsuario';
-    protected static $columns = ['email', 'password'];
+    protected static string $table = 'usuarios';
+    protected static string $idName = 'idUsuario';
+    protected static array $columns = ['email', 'password'];
 
 
     public ?int $idUsuario;
@@ -14,8 +14,9 @@ class User extends ActiveRecord{
 
     public function __construct(array $args)
     {
-        $this->idUsuario = $args['idUsuario'] ?? null;
-        $this->idUsuario = $args['email'] ?? '';
-        $this->idUsuario = $args['password'] ?? '';
+        $this->idUsuario = isset($args['idUsuario'])? 
+            filter_var($args['idUsuario']): null;
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
     }
 }

7f3e15250345589e4e24e05a9bd1c65d3022a9d8
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-08-04 14:08:39 -0600

    feat: Adds function to check if a User instance is able to authenticate.

--- models/User.php
+++ models/User.php
@@ -12,11 +12,24 @@ class User extends ActiveRecord{
     public string $email;
     public string $password;
 
-    public function __construct(array $args)
+    public function __construct(?array $args = [])
     {
         $this->idUsuario = isset($args['idUsuario'])? 
             filter_var($args['idUsuario']): null;
         $this->email = $args['email'] ?? '';
         $this->password = $args['password'] ?? '';
+    }
+
+    public function canAuthenticate(): bool{
+        $query = "SELECT * FROM " . static::$table . " WHERE email = '" . $this->email . "' LIMIT 1";
+
+        $result = static::$db->query($query);
+
+        if($result->num_rows === 0) return false;
+        
+        $user = static::createObject($result->fetch_all(MYSQLI_ASSOC)[0]);
+        
+        //Check if the given credentials match.
+        return password_verify($this->password, $user->password);
     }
 }

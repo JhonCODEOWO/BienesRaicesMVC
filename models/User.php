5a7699f0013e92995770d52e06940ceb0cd3e537
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-08-05 13:56:48 -0600

    refactor: Removes unnecessary logic.

--- models/User.php
+++ models/User.php
@@ -19,17 +19,4 @@ class User extends ActiveRecord{
         $this->email = $args['email'] ?? '';
         $this->password = $args['password'] ?? '';
     }
-
-    public function canAuthenticate(): bool{
-        $query = "SELECT * FROM " . static::$table . " WHERE email = '" . $this->email . "' LIMIT 1";
-
-        $result = static::$db->query($query);
-
-        if($result->num_rows === 0) return false;
-        
-        $user = static::createObject($result->fetch_all(MYSQLI_ASSOC)[0]);
-        
-        //Check if the given credentials match.
-        return password_verify($this->password, $user->password);
-    }
 }
